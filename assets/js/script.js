9d644920799da9446f3d7d37efb924cfbe6bdd4f
Antho <maxister@gmail.com>
2022-05-12 19:18:15 +0200

    responsive +

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
 //Créer une liste avec trois éléments (Pierre, Feuille, Ciseaux). Ajouter un bouton « Shifumi ! ». Lorsque l'on clique sur « Shifumi ! », choisir un élément au hasard (Pierre, Feuille ou Ciseaux). Le comparer à l'élément choisi par le joueur pour voir qui a gagné entre l'humain et la machine.
 //Bonus : Ajouter un compteur de victoires et de défaites et afficher le pourcentage de victoire contre l'ordinateur.
-let humanWinCount =0;
+let humanWinCount = 0;
 let humanLooseCount = 0;
 let numbTotalOfGame = 0;
 let shifuArray = ['pierre', 'feuille', 'ciseaux'];
 let humanWinRatio = 0;
 let winEgalOrLoose = '';
+// UN CODE BIEN INDENTé = UN THIERRY-PAS-ENERVé
+// UN CODE BIEN COMMENTé = UN THIERRY RELAXé :)
 
 //renvoi un nombre entier aléatoire entre 1 et 3
 function randomFrom0to3() {
     return Math.floor(Math.random() * 3);
 }
-//en fonction de randomFrom0to3 déduis quel est le choix de l'ordinateur
+//en fonction de randomFrom0to3 déduit quel est le choix de l'ordinateur
 function whichOneCompChoose() {
     let compNb = randomFrom0to3();
     let compChoice = '';
@@ -24,7 +26,7 @@ function whichOneCompChoose() {
             break;
         case 2:
             compChoice = shifuArray[2];
-            break;    
+            break;
         default:
             break;
     }
@@ -32,53 +34,56 @@ function whichOneCompChoose() {
 }
 // let choiceSelect = Array.from(document.getElementsByClassName('pushImg'));
 // console.log(choiceSelect);
+
 let choiceSelect = document.querySelectorAll('.pushImg');
+// Récupération de l'id de chaque class .pushImg afin de déduire le choix utilisateur
 choiceSelect.forEach(element => {
     element.addEventListener('click', (event) => {
-    let choosedOneByComp = whichOneCompChoose();
-    let choosedByHuman = event.target.id;
-    numbTotalOfGame += 1;
-    nbTotalGame.innerHTML = `Nombre total de partie(s) : ${numbTotalOfGame}`;
-    if (choosedByHuman == choosedOneByComp) {
-        winEgalOrLoose = 'Egalite!';
-    } else if (choosedByHuman == 'pierre' && choosedOneByComp == 'feuille') {
-        humanLooseCount += 1;
-        winEgalOrLoose = 'Perdu :(';
-        looseCount.innerHTML = `Nombre de defaite(s) : ${humanLooseCount}`;
-    } else if (choosedByHuman == 'pierre' && choosedOneByComp == 'ciseaux') {
-        humanWinCount += 1;
-        winEgalOrLoose = 'Victoire!';
-        winCount.innerHTML = `Nombre de victoire(s) : ${humanWinCount}`;
-    } else if (choosedByHuman == 'feuille' && choosedOneByComp == 'pierre') {
-        humanWinCount += 1;
-        winEgalOrLoose = 'Victoire!';
-        winCount.innerHTML = `Nombre de victoire(s) : ${humanWinCount}`;
-    } else if (choosedByHuman == 'feuille' && choosedOneByComp == 'ciseaux') {
-        humanLooseCount += 1;
-        winEgalOrLoose = 'Perdu :(';
-        looseCount.innerHTML = `Nombre de defaite(s) : ${humanLooseCount}`;
-    } else if (choosedByHuman == 'ciseaux' && choosedOneByComp == 'pierre') {
-        humanLooseCount += 1;
-        winEgalOrLoose = 'Perdu :(';
-        looseCount.innerHTML = `Nombre de defaite(s) : ${humanLooseCount}`;
-    } else if (choosedByHuman == 'ciseaux' && choosedOneByComp == 'feuille') {
-        humanWinCount += 1;
-        winEgalOrLoose = 'Victoire!';
-        winCount.innerHTML = `Nombre de victoire(s) : ${humanWinCount}`;
-    }
-    humanWinRatio =  (humanWinCount / numbTotalOfGame) * 100;
-    percentOfWin.innerHTML = `Votre % de victoire : ${humanWinRatio.toPrecision(2)} %`; //Raccourci à 4 charactères
-    //ajout des 2 choix (ordinateur-fromRight & humain-fromLeft)
-    insertCompChoose.innerHTML = `
+        let choosedOneByComp = whichOneCompChoose();
+        let choosedByHuman = event.target.id;
+        numbTotalOfGame += 1; // Maj du nombre total de parties
+        nbTotalGame.innerHTML = `Nombre total de partie(s) : ${numbTotalOfGame}`;
+        if (choosedByHuman == choosedOneByComp) {
+            winEgalOrLoose = 'Egalite!';
+        } else if (choosedByHuman == 'pierre' && choosedOneByComp == 'feuille') {
+            humanLooseCount += 1;
+            winEgalOrLoose = 'Perdu :(';
+            looseCount.innerHTML = `Nombre de defaite(s) : ${humanLooseCount}`;
+        } else if (choosedByHuman == 'pierre' && choosedOneByComp == 'ciseaux') {
+            humanWinCount += 1;
+            winEgalOrLoose = 'Victoire!';
+            winCount.innerHTML = `Nombre de victoire(s) : ${humanWinCount}`;
+        } else if (choosedByHuman == 'feuille' && choosedOneByComp == 'pierre') {
+            humanWinCount += 1;
+            winEgalOrLoose = 'Victoire!';
+            winCount.innerHTML = `Nombre de victoire(s) : ${humanWinCount}`;
+        } else if (choosedByHuman == 'feuille' && choosedOneByComp == 'ciseaux') {
+            humanLooseCount += 1;
+            winEgalOrLoose = 'Perdu :(';
+            looseCount.innerHTML = `Nombre de defaite(s) : ${humanLooseCount}`;
+        } else if (choosedByHuman == 'ciseaux' && choosedOneByComp == 'pierre') {
+            humanLooseCount += 1;
+            winEgalOrLoose = 'Perdu :(';
+            looseCount.innerHTML = `Nombre de defaite(s) : ${humanLooseCount}`;
+        } else if (choosedByHuman == 'ciseaux' && choosedOneByComp == 'feuille') {
+            humanWinCount += 1;
+            winEgalOrLoose = 'Victoire!';
+            winCount.innerHTML = `Nombre de victoire(s) : ${humanWinCount}`;
+        }
+        humanWinRatio = (humanWinCount / numbTotalOfGame) * 100;
+        percentOfWin.innerHTML = `Votre % de victoire : ${humanWinRatio.toPrecision(2)} %`; //Raccourci à 4 charactères
+        //ajout des 2 choix - (image) (ordinateur-fromRight & humain-fromLeft) - dans la div "insertCompChoose"
+        insertCompChoose.innerHTML = `
         <span id="resultText">${winEgalOrLoose}</span>
         <img src="assets/img/${choosedOneByComp}.png" class="fromRight" alt="Hands choosen by computer">
         <img src="assets/img/${choosedByHuman}.png" class="fromLeft" alt="Hands Choosen by human">
         `;
-    let saberSound = document.getElementById('saberSound');
-    setTimeout(() => {
-        saberSound.play();
-    }, 300);     
-});
+        //Envoi du son sabre lors de la "colision"
+        let saberSound = document.getElementById('saberSound');
+        setTimeout(() => {
+            saberSound.play();
+        }, 300);
+    });
 });
 
 // affichage et fermeture des modales
@@ -90,8 +95,8 @@ closeModal.addEventListener('click', () => {
 })
 stats.addEventListener('click', () => {
     statsModal.style.display = "block";
-}) 
-closeStatsModal.addEventListener('click', () =>{
+})
+closeStatsModal.addEventListener('click', () => {
     statsModal.style.display = "none";
 })
 startGame.addEventListener('click', () => {
